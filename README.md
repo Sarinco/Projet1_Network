e1b173b9ad54fd16cf30f54c4bca2a7772b8c740
adeleux <deleuxantoine@gmail.com>
2023-04-02 22:14:07 +0200

    avancement dans analyse dns et premiers graphs

--- README.md
+++ README.md
@@ -6,6 +6,36 @@
 
 -> Voir Messenger discussion structure
 
+### Ptites infos sur mes expériences
+
+- authentication 01  
+  - **nb paquets :** 1156
+- authentication 02  
+  - **nb paquets :** 502
+- call scenario 01 (la call avec tt les problèmes)  
+  - start record packets : 00:05
+  - start call : 00:19
+  - invite mathieu : 00:51
+  - mathieu joins with mic: 00:59
+  - mathieu turns mic off : 1:06
+  - antoine activates computer audio but shuts it off almost directly : 1:38
+  - antoine unmutes : 2:19
+  - mathieu unmuted : ? between 2:19 and 2:44
+  - antoine shared his screen : 3:48
+  - antoine stopped remote share : 4:51
+  - antoine stopped screensharing : 5:40
+  - both mathieu and antoine muted : 5:48
+  - end of meeting : 5:51
+  - antoine closed zoom app : 6:16
+  - end of packet recording : 6:23
+  - **nb paquets :** 15256
+- call scenario 02  (je trouve pas la vidéo pour celui la)
+  - **nb paquets :** 4259
+- essai messagerie  
+  - **nb paquets :** 104
+- screenshare  
+  - **nb paquets :** 7917
+
 ## TODO List
 
 ## 1. Scénarios
--- dns_analysis.py
+++ dns_analysis.py
@@ -1,5 +1,7 @@
 import pyshark as ps
 import numpy as np
+import matplotlib.pyplot as plt
+import sys
 from os import listdir
 from os.path import isfile
 
@@ -34,13 +36,71 @@ def CountDNSPackets(tracePath):
             
     capture.close()
     return domainCount
+
+def to_seconds(dt_time):
+    return 60*dt_time.minute + dt_time.seconds
+
+def graphmaker(traceList):
+    """Function that makes a bar graph of the number of packets per dns protocol
     
+    Args:
+        traceList (pcapng files): a list of packet trace
+    """
+    nbPacket = []
+    names = []
+    for traces in traceList:
+        nbPacket.append(CountDNSPackets(traces))
+        names.append(traces.split("/")[1].split("_")[1])
+    
+    plt.bar(names, nbPacket)
+    plt.xticks(rotation=30)
+    plt.title("Nombre de nom de domaines résolus par trace")
+    plt.tight_layout()
+    plt.savefig("graphs/dns_bar_graph.png")
+    
+    #plot a graph with the amount of dns packets per second for seven minutes
+    fig, ax = plt.subplots(len(traceList), 1, figsize=(10, 10))
+    for i in range(len(traceList)):
+        print("capture de :" + traceList[i])
+        timeOfPacket = np.zeros(60*7)
+        totalTime = [i for i in range(60*7)]
+        
+        captotal = ps.FileCapture(traceList[i])
+        firstPacketTime = captotal[0].sniff_time
+        captotal.close()
+        
+        cap = ps.FileCapture(traceList[i], display_filter='dns')
+        for pkt in cap:
+            timeOfPacket[int((pkt.sniff_time-firstPacketTime).total_seconds())] += 1
+        
+        ax[i].set_title(traceList[i].split("/")[1].split("_")[1])      
+        ax[i].plot(timeOfPacket)
+        ax[i].grid()
+
+        
+        
+        cap.close()
+    plt.tight_layout()
+    plt.savefig("graphs/dns_packetTime_graph.png")
     
 if __name__ == '__main__':
     onlyfiles = [f for f in listdir("packet_traces") if isfile("packet_traces/" + f)]
     # print(onlyfiles)
-    print("\n=======================================")
-    for traces in onlyfiles:
-        print("nom de domaines résolus: " + str(CountDNSPackets("packet_traces/" + traces)))
-        print("time of packet capture " + str(computeexperimentTime("packet_traces/" + traces))) 
-        print("=======================================") 
+    for i in range(len(onlyfiles)):
+        onlyfiles[i] = "packet_traces/" + onlyfiles[i]
+        
+    
+    if sys.argv[1] == "graph":
+        graphmaker(onlyfiles)
+    else:    
+        print("\n=======================================")
+        for traces in onlyfiles:
+            print("nom de domaines résolus: " + str(CountDNSPackets(traces)))
+            #print("time of packet capture " + str(computeexperimentTime(traces))) 
+            cap = ps.FileCapture(traces)
+            nbPacket = 0
+            for pkt in cap:
+                nbPacket += 1
+            print("nombre de paquets total: " + str(nbPacket))
+            cap.close()
+            print("=======================================") 
